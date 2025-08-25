1b82a50b13c695cfdeebb2ef5a53489c7a3dedf1
Kenneth J. Pronovici <pronovic@ieee.org>
2025-08-25 13:13:12 -0500

    Fixes for T201

--- src/tests/smartapp/test_converter.py
+++ src/tests/smartapp/test_converter.py
@@ -370,7 +370,6 @@ class TestStringSecrets:
         json = requests[source]
         request = CONVERTER.from_json(json, LifecycleRequest)
         for string in ["%s" % request, str(request), repr(request)]:
-            print(string)
             assert "auth_token" not in string and "authTokenValue" not in string
             assert "refresh_token" not in string and "refreshTokenValue" not in string
 
