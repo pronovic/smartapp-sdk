0c45f0cf8def0f342e619e1c93a77b06442c6c4d
Kenneth J. Pronovici <pronovic@ieee.org>
2025-08-25 11:33:02 -0500

    Replace Pylint with Ruff linter

--- src/tests/smartapp/test_signature.py
+++ src/tests/smartapp/test_signature.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # vim: set ft=python ts=4 sw=4 expandtab:
-# pylint: disable=redefined-outer-name,line-too-long:
 from typing import Dict, Optional
 from unittest.mock import patch
 
